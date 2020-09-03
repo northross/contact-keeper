7edafcce7e5870cf6ca9e5274d0dd805887df001
northross <rossnorthrup@yahoo.com>
2020-09-03 14:17:10 -0400

    hardcoding contacts for formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
+import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 3,
+        name: 'HARRY WHITE',
+        email: 'harry@gmail.com',
+        phone: '555-666-7777',
+        type: 'professional',
+      },
+      {
+        id: 2,
+        name: 'Susan WHITE',
+        email: 'susan@gmail.com',
+        phone: '111-666-7777',
+        type: 'professional',
+      },
+      {
+        id: 1,
+        name: 'Debbie WHITE',
+        email: 'debbie@gmail.com',
+        phone: '222-666-7777',
+        type: 'professional',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // add contact
+
+  // delete contact
+
+  // set current contact
+
+  // clear current contact
+
+  // update contact
+
+  // filter contacts
+
+  // clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
